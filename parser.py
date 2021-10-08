1b4e36fe743edf87bf064f7cbbd20c26c94171b8
Cristian Mendivil <cmendivil7@icloud.com>
2021-10-07 18:39:08 -0600

    Comments added and small changes to code

--- parser.py
+++ parser.py
@@ -3,18 +3,23 @@ import re
 
 
 def parser(lines):
+    '''
+    :param lines: an array of lines to be parsed
+    :return: a dictionary with all the valid data from the lines array
+    '''
     data = {}
     for line in lines:
         line = line.strip(" \n")
 
         if re.match('^#.', line):  # check for comments
-            pass
+            continue
         elif re.match('^([\w]+[ ]*)(=){1}([^=].)+',line):  # check for valid input
             value = line.split('=', 1)
             value[0] = value[0].strip()
             value[1] = value[1].strip()
 
             styled_value = None
+            # check variable type and save it correctly
             if re.match('true|on|yes', value[1]):
                 styled_value = True
             elif re.match('false|off|no', value[1]):
@@ -27,9 +32,8 @@ def parser(lines):
                 styled_value = value[1]
 
             data[value[0]] = styled_value
-        else:
-            raise Exception("Invalid data \'%s\'" % line)
-            pass
+        else:  # Raise exeption in case of invalid data
+            raise Exception("Invalid data - \'%s\'" % line)
 
     return data
 
@@ -38,8 +42,9 @@ def main():
     if sys.argv[1] is None:
         return
 
-    with open(sys.argv[1], 'r') as f:
-        lines = f.readlines()
+    with open(sys.argv[1], 'r') as file:
+        lines = file.readlines()
+    file.close()
 
     data = parser(lines)
 
